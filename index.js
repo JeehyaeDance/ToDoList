424f0f94a6e8536bc371a1fafc53de4cb528bd6d
Jeehyae dance <jeehyae91@gmail.com>
2019-06-04 16:20:44 -0700

    adding x mark to new list

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//adding x mark
 var myList = document.getElementsByTagName("li");
 for(let i = 0; i < myList.length; i++){
 	var span = document.createElement("span");
@@ -7,6 +8,7 @@ for(let i = 0; i < myList.length; i++){
 	myList[i].appendChild(span);
 }
 
+//closing action
 var close = document.getElementsByClassName("close");
 for(let i = 0; i < close.length; i++){
 	close[i].onclick = function(){
@@ -15,6 +17,7 @@ for(let i = 0; i < close.length; i++){
 	}
 }
 
+//adding a new list
 var form = document.getElementById('addForm');
 console.log(form)
 form.addEventListener('submit', newList);
@@ -32,6 +35,20 @@ function newList(e){
 	else {
 		document.getElementById("list").appendChild(li);
 	}
+
+	var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+  	 span.className = "close";
+  	 span.appendChild(txt);
+  	 li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+    	 close[i].onclick = function() {
+      	 var div = this.parentElement;
+     	 div.style.display = "none";
+    }
+  }
 }
+
 
 
